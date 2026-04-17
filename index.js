9593797bad0d19d0d0bb137eca3a7a856c57194b
ljhaesler <lithiumium@gmail.com>
2026-04-17 15:12:06 +0200

    interesting 3d visual

--- index.js
+++ index.js
@@ -65,19 +65,19 @@ root.origin.set(root.width / 2, root.height / 2);
 root.addChild(...blobContainers);
 
 app.ticker.add(() => {
-  t1 += 0.002;
-  t2 += 0.004;
+  t1 += 0.016;
+  t2 += 0.002;
 
   for (const container of blobContainers) {
     // root.rotation += 0.0001;
     container.orbitAngle += container.orbitSpeed;
     container.x =
       centerX +
-      Math.cos(container.orbitAngle + container.orbitSpeed * 8192 + t1) *
+      Math.cos(container.orbitAngle + container.orbitSpeed * 512 + t1) *
         container.orbitRadius;
     container.y =
       centerY +
-      Math.sin(container.orbitAngle + container.orbitSpeed * 4096 - t2) *
+      Math.sin(container.orbitAngle + container.orbitSpeed * 8192 - t2) *
         container.orbitRadius;
     container.rotation += container.rotationSpeed;
   }
